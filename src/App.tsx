e671b211ce5b6bea764513d71270a7c44976e79d
Andrii Yanchuk <horeca288@gmail.com>
2026-04-21 21:06:24 +0300

    feat: added bet controls component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { useEffect } from "react";
 import { useSlotStore } from "./store/useSlotStore";
 import { SlotMachine } from "./components/SlotMachine";
+import { BetControls } from "./components/BetControls";
 
 function App() {
   const {
@@ -28,7 +29,7 @@ function App() {
         <img
           src="/public/label.webp"
           alt="label"
-          className="w-[228px] h-[46px] relative"
+          className="w-57 h-11.5 relative"
         />
         <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text">
           Tokyo Slots
@@ -36,7 +37,6 @@ function App() {
       </div>
 
       <div className="relative flex flex-col items-center gap-6 max-w-md w-full">
-        {/* Slot Machine */}
         <SlotMachine reels={reels} />
 
         {/* Win display */}
@@ -47,32 +47,13 @@ function App() {
         )}
 
         {/* Bet Controls */}
-        <div className="flex flex-col items-center gap-2">
-          <span className="text-gray-600 text-sm font-medium tracking-wider">
-            PLACE A BET
-          </span>
-          <div className="flex items-center gap-2">
-            <button
-              onClick={decrementBet}
-              disabled={spinning || bet <= 100}
-              className="w-10 h-10 bg-gray-300 hover:bg-gray-400 disabled:opacity-50 rounded-full flex items-center justify-center transition-colors"
-            >
-              -
-            </button>
-            <div className="bg-orange-400 px-6 py-2 rounded-full min-w-35 text-center">
-              <span className="text-white font-bold">
-                {bet.toLocaleString()}
-              </span>
-            </div>
-            <button
-              onClick={incrementBet}
-              disabled={spinning || bet >= balance}
-              className="w-10 h-10 bg-gray-300 hover:bg-gray-400 disabled:opacity-50 rounded-full flex items-center justify-center transition-colors"
-            >
-              +
-            </button>
-          </div>
-        </div>
+        <BetControls
+          decrementBet={decrementBet}
+          incrementBet={incrementBet}
+          bet={bet}
+          spinning={spinning}
+          balance={balance}
+        />
 
         {/* Spin Button */}
         <button
---
+++ src/components/BetControls.tsx
@@ -0,0 +1,50 @@
+interface BetControlsProps {
+  decrementBet: () => void;
+  incrementBet: () => void;
+  bet: number;
+  spinning: boolean;
+  balance: number;
+}
+
+export function BetControls(props: BetControlsProps) {
+  const { decrementBet, incrementBet, bet, spinning, balance } = props;
+
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <span className="text-gray-600 text-sm font-medium tracking-wider">
+        PLACE A BET
+      </span>
+      <div className="flex items-center gap-2">
+        <button
+          onClick={decrementBet}
+          disabled={spinning || bet <= 100}
+          className="w-16 h-16 rounded-2xl border-2 border-[#341D1A] bg-[#FEFEFF] text-[#341D1A] text-xl font-bold flex items-center justify-center shadow-[0_6px_0_#515895] transition-all duration-100 active:translate-y-1.5 active:shadow-[0_0px_0_#515895] disabled:opacity-50 disabled:translate-y-1.5 disabled:shadow-[0_0px_0_#515895]"
+        >
+          -
+        </button>
+
+        <div className="relative">
+          <img src="/bet-place.png" alt="bet place" />
+
+          <div className="absolute top-7 left-0 w-full flex items-center justify-between px-4">
+            <div className="flex h-6 w-6 items-center justify-center rounded-full bg-[#FF315F] text-black font-black text-sm border-[5px] border-[#341D1A] shadow-[0_4px_0_#5B5354]">
+              T
+            </div>
+
+            <span className="text-[20px] leading-none font-black text-white [text-shadow:0_4px_0_#1F1F1F]">
+              {bet.toLocaleString()}
+            </span>
+          </div>
+        </div>
+
+        <button
+          onClick={incrementBet}
+          disabled={spinning || bet >= balance}
+          className="w-16 h-16 rounded-2xl border-2 border-[#341D1A] bg-[#FEFEFF] text-[#341D1A] text-xl font-bold flex items-center justify-center shadow-[0_6px_0_#515895] transition-all duration-100 active:translate-y-1.5 active:shadow-[0_0px_0_#515895] disabled:opacity-50 disabled:translate-y-1.5 disabled:shadow-[0_0px_0_#515895]"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
